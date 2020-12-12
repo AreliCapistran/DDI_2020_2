8d190339000a7bc286fa3d74dc4904fd58904431
AreliCapistran <71529904+AreliCapistran@users.noreply.github.com>
2020-12-11 16:54:35 -0800

    Practica 8
    Comando de voz

---
+++ Practica8/Assets/Color.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : VoiceInteractable
+{
+    public Material[] materials;
+    public Renderer myRenderer;
+
+    public override void VoiceInteract(string action)
+    {
+        Debug.Log("Ropa nueva!");
+        if (action == "change")
+        {
+            int index = Random.Range(0, materials.Length);
+            myRenderer.material = materials[index];
+        }
+    }
+}
